ba7bc1a1f754afdca9469d13619d36ab40ac6c2d
gdp311201 <growspheredigpro@gmail.com>
2026-09-08 15:37:11 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,24 @@
+/**
+ * js/config.js
+ * Master Configuration Data
+ */
+
+export const CONFIG = {
+  COUPLE_NAME: "Samuel & Evelyn",
+  eventDate: "2026-10-24T09:00:00+07:00",
+  couple: {
+    groom: {
+      fullName: "Samuel, S.T.",
+      shortName: "Samuel"
+    },
+    bride: {
+      fullName: "Evelyn, S.E.",
+      shortName: "Evelyn"
+    }
+  },
+  gift: {
+    physicalGift: {
+      phone: "6281234567890"
+    }
+  }
+};
