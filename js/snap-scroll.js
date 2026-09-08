c5bbb9ff201f1658db1395db05c00c92fccdb72e
gdp311201 <growspheredigpro@gmail.com>
2026-09-08 15:43:10 +0700

    Create snap-scroll.js

---
+++ js/snap-scroll.js
@@ -0,0 +1,12 @@
+/**
+ * js/snap-scroll.js
+ * Handler Scroll Lock / Unlock
+ */
+
+export function unlockScroll() {
+    document.body.style.overflow = 'auto';
+}
+
+export function initSnapScroll() {
+    console.log("Snap scroll initialized");
+}
